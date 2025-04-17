36ba02ec92d54f85d998e071fd1b302d82f297f1
salimdotpy <salimdotpy@gmail.com>
2025-04-17 13:35:57 +0100

    new update

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import MainLayout from "../ui/container";
-import { AboutSection, ContactSection, FooterSection, HeroSection, PaymentSection, ServiceSection } from "../ui/sections";
+import { AboutSection, CatchySection, ContactSection, FooterSection, HeroSection, PaymentSection, ServiceSection } from "../ui/sections";
 
 export default function Index() {
     return (
         <MainLayout sitename={'Deran More'}>
             <HeroSection />
             <AboutSection />
+            <CatchySection />
             <ServiceSection />
             <PaymentSection />
             <ContactSection />
