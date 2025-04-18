32c09130cf2e66e71c973a08f3c62216cb17c5a8
salimdotpy <salimdotpy@gmail.com>
2025-04-18 15:27:33 +0100

    new update

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import MainLayout from "../ui/container";
 import { AboutSection, CatchySection, ContactSection, FooterSection, HeroSection, PaymentSection, ServiceSection } from "../ui/sections";
+import { useDocumentTitle } from "../hooks";
 
 export default function Index() {
+    useDocumentTitle('Deran More');
     return (
         <MainLayout sitename={'Deran More'}>
             <HeroSection />
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,9 @@
 import React from "react";
 import { AuthLayout } from "../ui/container";
 import AdminLogin from "../ui/admin/login";
+import { useDocumentTitle } from "../hooks";
 export default function Login() {
+    useDocumentTitle('Login - DeranMore')
     return (
         <AuthLayout phrase="Hi, Welcome Back">
             <AdminLogin />
